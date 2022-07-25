9699360ea0d7fce119f25c8b9882876e1b52aec2
Gujju Aditya <guzzuaditya@gmail.com>
2022-07-25 23:37:32 +0530

    party house function modified

--- partyhouse.cpp
+++ partyhouse.cpp
@@ -13,6 +13,5 @@ void resort_transaction(player *player_array[],player *current_player,int number
 		for(int i=0; i<number_of_players && i != current_player->player_number; i++)
 			{
 				player_array[i]->balance -= 200;
-			}
-				
+			}		
 	}	
