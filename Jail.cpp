402c885c06068a3bec2d13ac742bb089404eb1d0
Gujju Aditya <guzzuaditya@gmail.com>
2022-07-27 15:20:00 +0530

    Improved the jail class function by debugging it

--- Jail.cpp
+++ Jail.cpp
@@ -1,5 +1,10 @@
+/* ALGORITHM:
+   1.) Increment the visits variable by one
+   2.) Debit 500 from the current player's balance
+*/
+
 #include "class_definitions.h"
-void jail_transaction(player current_player)
+void jail_class::transaction(player &current_player)
 {
 	visits++;
 	current_player.balance -= 500;
