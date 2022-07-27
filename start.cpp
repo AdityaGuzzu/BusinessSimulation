4e41a31868e6456e084f8977d2ca1c4e9fd5a074
Gujju Aditya <guzzuaditya@gmail.com>
2022-07-27 11:56:40 +0530

    Defined, documented and declared the start function

--- start.cpp
+++ start.cpp
@@ -1,11 +1,19 @@
 /* This file will add 2000 to the balance of the player if he crossed start.
 	ALGORITHM:
-	1.) Take the player's pointer as an argument
-	2.) Take the 'rounds' vector which has the round number of the player after every throw
-	3.) Check the last two elements. If they differ by one, add 2000 to his balance
+	1.) Take the current player's reference as an argument
+	2.) In the 'rounds' vector which has the round number of the player after every throw,
+		 Check the last two elements. If they differ by one, credit 2000 to the balance.
+	4) If the player is exactly on start, credit 2000 to the balance
 */
 #include "class_defintions.h"
-void start_class::transaction(player *current_player)
+void start_class::transaction(player &current_player)
 {
-
+	if((current_player.position%36) == 0)
+	{
+		visits ++;
+	}
+	if(current_player.rounds.back() - current_player.rounds(rounds.size()-1) == 1)
+	{
+		current_player.balance += 2000;
+	}
 }
