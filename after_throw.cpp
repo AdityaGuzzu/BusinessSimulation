480e718f8c18ab4784d678c7913bda85ead7e6d9
Gujju Aditya <guzzuaditya@gmail.com>
2022-08-01 16:07:35 +0530

    Added after_throw function

--- after_throw.cpp
+++ after_throw.cpp
@@ -3,7 +3,14 @@
     ---> assign it to throw_ variable of current player
     ---> append throw_ to current player's throws vector
     ---> current player's position = ( (position + throw_) % 36 )
-    ---> add throw_ to the positions variable
+    ---> add throw_ to the blocks_covered variable
     ---> append ( (positions) /36 ) to rounds vector
 */
-    
+#include<iostream>
+#include "class_definitions.h"
+void after_throw(player *current_player)
+{
+    current_player->blocks_covered += current_player->throw_;
+    current_player->round = (current_player->blocks_covered / 36);
+    current_player->rounds.push_back(current_player->round);
+}    
